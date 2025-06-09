72ba0c012e8bfddf94b56819b45802a1872a7af9
Sateesh <sateesh@qualminds.com>
2025-06-09 17:21:29 +0530

    feat(.htaccess): Add rewrite rules for improved routing support

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> /!* Wrap App with BrowserRouter *!/ */}
-    <HashRouter> {/* Wrap App with HashRouter */}
+    <BrowserRouter> {/* Wrap App with BrowserRouter */}
       <App />
-    {/* </BrowserRouter> */}
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

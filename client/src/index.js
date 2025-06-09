2e5e7156dea9600b555271f1f59c036720ad1e8d
Sateesh <sateesh@qualminds.com>
2025-06-09 16:20:46 +0530

    feat: Replace BrowserRouter with HashRouter for improved routing compatibility

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter> { /* Wrap App with BrowserRouter */ }
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    {/* <BrowserRouter> /!* Wrap App with BrowserRouter *!/ */}
+    <HashRouter> {/* Wrap App with HashRouter */}
+      <App />
+    {/* </BrowserRouter> */}
+    </HashRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
